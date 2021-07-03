c897d65f40fe34e60a833168c7ff6a9aa593ad8d
adrianammt <amorat@gmx.de>
2021-07-03 17:33:47 +0200

    Add css that is common to all pages to utility, rename js fil to script, rename submit button in form and bookmark icon name, add a css to base

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 //-- For the Landingpage and the Bookmark --
 
 const showHideButton = document.querySelectorAll(".show-hide-answer");
+
 const showAnswer = document.querySelectorAll(".answer-text");
-const bookmark = document.querySelector("");
+const bookmark = document.querySelectorAll(".quiz-card__bookmark");
 
 for (let i = 0; i < showHideButton.length; i++) {
   showHideButton[i].addEventListener("click", () => {
@@ -17,15 +18,8 @@ for (let i = 0; i < showHideButton.length; i++) {
   });
 }
 
-for (let i = 0; i < bookmark.length; i++) {
-  showHideButton[i].addEventListener("click", () => {
-    const show = showAnswer[i].classList.toggle("show-text");
-    if (showHideButton[i].textContent === "Show answer") {
-      showHideButton[i].textContent = "Hide answer";
-      showHideButton[i].classList.add("hideanswer");
-    } else {
-      showHideButton[i].textContent = "Show answer";
-      showHideButton[i].classList.remove("hideanswer");
-    }
+for (let k = 0; k < bookmark.length; k++) {
+  bookmark[k].addEventListener("click", () => {
+    bookmark[k].classList.toggle("quiz-card__bookmark--active");
   });
 }
